73b251ba90322eebb5f12a6b800bf09f89a2ff7b
MD. Mahidul Islam <mmbmahidul007@gmail.com>
2020-11-01 22:53:48 +0600

    Single Context

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 import IntervalClassCounter from "./components/IntervalClassCounter";
 import IntervalHookCounter from "./components/IntervalHookCounter";
 import DataFetching from "./components/DataFetching";
+import ComponentC from "./components/ComponentC";
+
+export const UserContext=React.createContext()
 
 function App() {
   return (
     <div className="App">
-      <DataFetching/>
+      <UserContext.Provider value={'Mahidul'}>
+          <ComponentC/>
+      </UserContext.Provider>
     </div>
   );
 }
