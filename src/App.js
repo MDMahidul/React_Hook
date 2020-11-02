ab4b9225cdff7d5b173fb9673a385072fe2ce7ae
MD. Mahidul Islam <mmbmahidul007@gmail.com>
2020-11-02 15:38:27 +0600

    useReducer One

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import './App.css';
-import IntervalClassCounter from "./components/IntervalClassCounter";
-import IntervalHookCounter from "./components/IntervalHookCounter";
-import DataFetching from "./components/DataFetching";
-import ComponentC from "./components/ComponentC";
+import CounterOneuseReducer from "./components/CounterOneuseReducer";
 
-export const UserContext=React.createContext()
-export const ChannelContext=React.createContext()
 
 function App() {
   return (
     <div className="App">
-      <UserContext.Provider value={'Mahidul'}>
-          <ChannelContext.Provider value={'Islam'}>
-              <ComponentC/>
-          </ChannelContext.Provider>
-      </UserContext.Provider>
+        <CounterOneuseReducer/>
     </div>
   );
 }
