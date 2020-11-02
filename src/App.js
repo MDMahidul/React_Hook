25536136082fdbd588009bbe7a70b0b6559b9973
MD. Mahidul Islam <mmbmahidul007@gmail.com>
2020-11-02 22:55:12 +0600

    useMemo Hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React,{useReducer} from 'react';
+import React from 'react';
 import './App.css';
 import ParentComponent from "./components/ParentComponent";
+import Counter from "./components/Counter";
 function App() {
  return (
       <div className="App">
-            <ParentComponent/>
+            <Counter/>
       </div>
   );
 }
