b1c264beaf6cebb2780da998db2ef313e722daea
MD. Mahidul Islam <mmbmahidul007@gmail.com>
2020-11-02 16:22:04 +0600

    Multiple useReduce

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import CounterOneuseReducer from "./components/CounterOneuseReducer";
 import CounterTwouseReducer from "./components/CounterTwouseReducer";
+import CounterThreeuseReducer from "./components/CounterThreeuseReducer";
 
 
 function App() {
   return (
     <div className="App">
-        <CounterTwouseReducer/>
+        <CounterThreeuseReducer/>
     </div>
   );
 }
